42c3d1fb6cb3adcd162b976d7a20f00c3059eda1
gsinthujan@yahoo.com <gsinthujan@yahoo.com>
2016-11-05 04:04:11 +0530

    Made changes to admistrative controllers

--- src/main/java/com/sliit/smartlady/controller/IndexController.java
+++ src/main/java/com/sliit/smartlady/controller/IndexController.java
@@ -1,24 +1,15 @@
 package com.sliit.smartlady.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
 
-	  @RequestMapping(value = "/" , method = RequestMethod.GET)
-	public String getIndexPage() {
-		return "ReaderIndex";
-	}
-
-
-
-
-
-
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getIndexPage() {
+        return "index";
+    }
+    
 }
